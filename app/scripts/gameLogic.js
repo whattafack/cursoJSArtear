3e8186801cc47dd6d2598a34b510dba28e3aefcd
Francisco <franciscok@artear.com>
2016-03-30 16:11:58 -0300

    new player

--- app/scripts/gameLogic.js
+++ app/scripts/gameLogic.js
@@ -13,20 +13,66 @@ var optionsSamid = {
   }
 };
 
+var PlayerHandler = function (player1, player2) {
+  this.manageGame = function() {
+    if(player1.turno){
+      player1.doTurno();
+    }else {
+      player2.doTurno();
+    }
+
+  }
+};
+
+var Player = function(optionPersonaje) {
+  var turno = true;
+  var personaje = new Personaje(optionPersonaje);
+  personaje.init();
+
+  this.doTurno = function() {
+    var option = this.launchOptions();
+    this.turno = false;
+  };
+
+  this.launchOptions = function() {
+    var option = "ataque";
+    bootbox.dialog({
+      message: "¿Qué queré hacer?" + personaje.nombre,
+      title: "Combate Space",
+      onEscape: function() {
+      },
+      show: true,
+      backdrop: true,
+      closeButton: true,
+      animate: true,
+      className: "my-modal",
+      buttons: {
+        "Atacar": function() {personaje.ataque()},
+        "Defender": function() {personaje.defensa()}
+      }
+    });
+
+  };
+
+};
 
 
 // Init general
 $(document).ready(function(){
-  samid = new Personaje(optionsSamid);
-  samid.init();
+  bootbox.dialog({
+    message: "¿Vamos a Juegar?",
+    title: "Combate Space",
+    show: true,
+    backdrop: true,
+    closeButton: true,
+    animate: true,
+    buttons: {
+      "Juegar": function() {
+        var player1 = new Player(optionsSamid);
+        var player2 = new Player(optionsSamid);
+        var playerHandler = new PlayerHandler(player1, player2);
+        playerHandler.manageGame();
+      }
+    }
+  });
 });
-
-
-/*
-Confirm para turnos:
-
-bootbox.confirm("Are you sure?", function(result) {
-  Example.show("Confirm result: "+result);
-}); 
-
-*/
